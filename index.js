f347d354368aff4517bd24273a1d227430bd9dfe
Matheus <matheustrilhakoch@hotmail.com>
2024-01-06 11:15:52 -0300

    Setting user and its id

--- index.js
+++ index.js
@@ -7,12 +7,37 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
 
 app.use(cors())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  }
+});
 
+const userModel = mongoose.model('user', userSchema);
+
+app.route('/api/users')
+.post(function(req, res) {
+  let userName = req.body.username;
+  var newUser = new userModel({
+    username: userName
+  });
+
+  newUser.save();
+
+  res.json({
+    username: userName,
+    _id: newUser._id
+  });
+
+});
 
 
 
