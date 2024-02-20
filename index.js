924c01c630d9493d1d7146d56d38ed086f67bd55
Matheus <matheustrilhakoch@hotmail.com>
2024-02-19 22:15:37 -0300

    Testing schemas

--- index.js
+++ index.js
@@ -18,10 +18,36 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true
+  },
+  logs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Exercise'
+  }]
+});
+
+const userModel = mongoose.model('User', userSchema);
+
+const exerciseSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
   }
 });
 
-const userModel = mongoose.model('user', userSchema);
+const exerciseModel = mongoose.model('Exercise', exerciseSchema);
 
 app.route('/api/users')
 .get(function(req, res) {
@@ -46,142 +72,69 @@ app.route('/api/users')
   newUser.save((err) => {
     if (err) {
       console.error("Error creating user:", err);
+      return res.status(500).send("Error creating user");
     }
-  });
 
-  res.json({
-    username: userName,
-    _id: newUser._id
+    res.json({
+      username: userName,
+      _id: newUser._id
+    });
   });
 });
-
-const exerciseSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true
-  },
-  duration: {
-    type: Number,
-    required: true
-  },
-  date: {
-    type: String,
-    required: true
-  },
-  date_log: {
-    type: String,
-    required: true
-  }
-});
-
-const exerciseModel = mongoose.model('exercise', exerciseSchema);
 
 app.route('/api/users/:_id/exercises')
 .post(function(req, res) {
     var userID = req.params._id;
 
-    var userName = userModel.find({_id: userID}).exec();
-    userName.then(function(doc) {
+    var userName = userModel.findById(userID).exec();
+    userName.then(function(user) {
       var desc = req.body.description;
       var dur =  req.body.duration;
-      var user = doc[0].username.toString();
-      var id = doc[0]._id.toString();
 
-      var dateFinal = req.body.date;
-
-        if (!dateFinal) {
-          dateFinal = (new Date()).toDateString();
-        } else if (dateFinal == undefined) {
-          dateFinal = (new Date()).toDateString();
-        } else {
-          dateFinal = (new Date(req.body.date)).toDateString();
-        }
+      var dateFinal = req.body.date ? (new Date(req.body.date)).toDateString() : (new Date()).toDateString();
 
       var newExercise = new exerciseModel({
         description: desc,
         duration: dur,
         date: dateFinal,
-        date_log: new Date()
+        userId: userID
       });
 
       newExercise.save((err) => {
         if (err) {
           console.error("Error creating exercise:", err);
+          return res.status(500).send("Error creating exercise");
         }
-      });
-      
-      var newExerciseReturn = exerciseModel.find().sort({date_log: -1}).limit(1).exec();
-      newExerciseReturn.then(function(doc) {
-        var desc = doc[0].description.toString();
-        var durNum = parseInt(doc[0].duration);
 
-        res.json({
-          _id: id,
-          username: user,
-          description: desc,
-          duration: durNum,
-          date: dateFinal
+        user.logs.push(newExercise);
+        user.save((err) => {
+          if (err) {
+            console.error("Error updating user:", err);
+            return res.status(500).send("Error updating user");
+          }
+
+          res.json(user);
         });
-        // types test
-        // console.log(
-        //   typeof(user),
-        //   typeof(desc),
-        //   typeof(durNum),
-        //   typeof(dateFinal),
-        //   typeof(id),
-        // )
-      })
+      });
     });
 });
 
-app.route('/api/users/:_id/logs/:from?/:to?/:limit?')
+app.route('/api/users/:_id/logs')
 .get(function(req, res) {
   var userId = req.params._id;
-  var searchParamsFrom = req.params.from;
-  var searchParamsTo = req.params.to;
-  var searchParamsLimit = req.params.limit;
 
-  var userName = userModel.find({_id: userId}).exec();
-  userName.then(function(doc) {
-    
-    var id = doc[0]._id.toString();
-    var user = doc[0].username;
-    
+  var userPromise = userModel.findById(userId).populate('logs').exec();
+  userPromise.then(function(user) {
+    var log = user.logs.map(exercise => ({
+      description: exercise.description,
+      duration: exercise.duration,
+      date: (new Date(exercise.date)).toDateString()
+    }));
 
-    var exerciseByUser = exerciseModel.find({username: user}, {
-      _id: 0,
-      description: 1,
-      duration: 1,
-      date: 1
-    }).exec()
-    exerciseByUser.then(function(docExercise) {
-
-      let docExerciseFilter = docExercise;
-      let docExerciseFiltered = docExercise;
-
-      if (searchParamsFrom && searchParamsTo) {
-        let fromDate = new Date(searchParamsFrom);
-        let toDate = new Date(searchParamsTo);
-        docExerciseFiltered = docExerciseFiltered.filter((doc) => new Date(doc.date) >= fromDate);
-        docExerciseFiltered = docExerciseFiltered.filter((doc) => new Date(doc.date) <= toDate);
-      }
-      
-      if (searchParamsLimit) {
-        docExerciseFiltered = docExerciseFiltered.slice(0, searchParamsLimit);
-      }
-
-      var countRes = docExerciseFiltered.length;
-
-      res.json({
-        _id: id,
-        username: user,
-        count: countRes,
-        log: docExerciseFiltered.map(exercise => ({
-          description: exercise.description,
-          duration: exercise.duration,
-          date: new Date(exercise.date).toDateString()
-        }))
-      });
+    res.json({
+      _id: user._id,
+      username: user.username,
+      log: log
     });
   })
 });
