edff1c18c8333e516c0198a16a64ff68aa5475bb
Matheus <matheustrilhakoch@hotmail.com>
2024-02-20 12:24:22 -0300

    Test2

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -19,25 +18,47 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true
-  }
+  },
+  exercises: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'exercise'
+  }]
 });
 
 const userModel = mongoose.model('user', userSchema);
 
+const exerciseSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  }
+});
+
+const exerciseModel = mongoose.model('exercise', exerciseSchema);
+
 app.route('/api/users')
-  .get(function(req, res) {
+  .get((req, res) => {
     userModel.find({}, (err, users) => {
       if (err) {
-        console.error("Error fetching users:", err);
+        console.error("Error finding users:", err);
         return res.status(500).json({ error: "Internal Server Error" });
       }
-      res.json(users.map(user => ({
+      const userQueryArray = users.map(user => ({
         username: user.username,
         _id: user._id
-      })));
+      }));
+      res.json(userQueryArray);
     });
   })
-  .post(function(req, res) {
+  .post((req, res) => {
     const { username } = req.body;
     const newUser = new userModel({ username });
 
@@ -46,18 +67,12 @@ app.route('/api/users')
         console.error("Error creating user:", err);
         return res.status(400).json({ error: "Could not create user" });
       }
-      res.json({ username: savedUser.username, _id: savedUser._id });
+      res.json({
+        username: savedUser.username,
+        _id: savedUser._id
+      });
     });
   });
-
-const exerciseSchema = new mongoose.Schema({
-  username: String,
-  description: String,
-  duration: Number,
-  date: String
-});
-
-const exerciseModel = mongoose.model('exercise', exerciseSchema);
 
 app.post('/api/users/:_id/exercises', (req, res) => {
   const { description, duration, date } = req.body;
@@ -88,91 +103,16 @@ app.post('/api/users/:_id/exercises', (req, res) => {
           return res.status(500).json({ error: "Internal Server Error" });
         }
 
+        const { _id, username } = savedUser;
+        const { description, duration, date } = savedExercise;
+
         res.json({
-          _id: savedUser._id,
-          username: savedUser.username,
-          description: savedExercise.description,
-          duration: savedExercise.duration,
-          date: savedExercise.date
+          _id,
+          username,
+          description,
+          duration,
+          date
         });
-      });
-    });
-  });
-});
-// app.post('/api/users/:_id/exercises', (req, res) => {
-//   const { description, duration, date } = req.body;
-//   const { _id } = req.params;
-
-//   userModel.findById(_id, (err, user) => {
-//     if (err || !user) {
-//       console.error("Error finding user:", err);
-//       return res.status(404).json({ error: "User not found" });
-//     }
-
-//     const newExercise = new exerciseModel({
-//       username: user.username,
-//       description,
-//       duration,
-//       date: date ? new Date(date).toDateString() : new Date().toDateString()
-//     });
-
-//     newExercise.save((err, savedExercise) => {
-//       if (err) {
-//         console.error("Error creating exercise:", err);
-//         return res.status(400).json({ error: "Could not create exercise" });
-//       }
-
-//       res.json({
-//         username: user.username,
-//         description: savedExercise.description,
-//         duration: savedExercise.duration,
-//         date: savedExercise.date,
-//         _id: user._id
-//       });
-//     });
-//   });
-// });
-
-app.get('/api/users/:_id/logs', (req, res) => {
-  const { _id } = req.params;
-  const { from, to, limit } = req.query;
-
-  userModel.findById(_id, (err, user) => {
-    if (err || !user) {
-      console.error("Error finding user:", err);
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    exerciseModel.find({ username: user.username }, (err, exercises) => {
-      if (err) {
-        console.error("Error fetching exercises:", err);
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
-
-      let log = exercises.map(exercise => ({
-        description: exercise.description,
-        duration: exercise.duration,
-        date: exercise.date
-      }));
-
-      if (from && to) {
-        const fromDate = new Date(from);
-        const toDate = new Date(to);
-        log = log.filter(exercise => {
-          const exerciseDate = new Date(exercise.date);
-          return exerciseDate >= fromDate && exerciseDate <= toDate;
-        });
-      }
-
-      if (limit) {
-        log = log.slice(0, limit);
-      }
-
-      res.json({
-        _id: user._id,
-        username: user.username,
-        count: log.length,
-        log
       });
     });
   });
